dec705f6301b55aab6be9bda9da2941e88df73ea
NIKHIL RANJAN KUMAR <88980648+niku-19@users.noreply.github.com>
2023-07-20 18:20:34 +0530

    build button components

--- src/PreBuiltComponents/Button/Button.jsx
+++ src/PreBuiltComponents/Button/Button.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 //styles import
-import "./Button.css";
+import styles from "./Button.module.css";
 import CodeShare from "../../Components/Code Share/CodeShare";
 const Button = () => {
   return (
-    <div className="container">
-      <h1 className="button__header">Button</h1>
-      <h2 className="button__subHeader">
+    <div className={styles.container}>
+      <h1 className={styles.button__header}>Button</h1>
+      <h2 className={styles.button__subHeader}>
         The Button component is used to trigger an action or event, such as
         submitting a form, opening a dialog, canceling an action, or performing
         a delete operation.
       </h2>
-      <h1>Usages</h1>
-      <div className="button__showCase__container">
+      {/* all button variants are applied by using the .btn class. This class will */}
+      <h1 className={styles.title}>Variants</h1>
+      <p className={styles.about__title}>
+        CSSENSE includes several button variants, each serving its own semantic
+        purpose, with a few extras thrown in for more control.
+      </p>
+      <div className={styles.button__showCase__container}>
         <button className="btn">Default</button>
         <button className="btn btn-primary">Primary</button>
         <button className="btn btn-secondary">Secondary</button>
@@ -23,6 +28,9 @@ const Button = () => {
         <button className="btn btn-light">Light</button>
         <button className="btn btn-dark">Dark</button>
       </div>
+      <p className={styles.copy__code__des}>
+        To achieve the above buttons, you need to follow the following code:
+      </p>
       <CodeShare
         highlight="1,3,5,7,9,11,13,15,17,19"
         code={`
@@ -44,6 +52,276 @@ const Button = () => {
         <button className="btn btn-light">Light</button>
                 < -- For Dark Button -->
         <button className="btn btn-dark">Dark</button>
+        `}
+      />
+      {/* all button variants are applied by using the .btn class. This class will */}
+      <h1>Outline buttons </h1>
+      <p className={styles.about__title}>
+        In need of a button, but not the hefty background colors they bring?
+        Replace the default modifier classes with the .btn-outline-* ones to
+        remove all background images and colors on any button.
+      </p>
+      <div className={styles.button__showCase__container}>
+        <button type="button" className="btn btn-outline-primary">
+          Primary
+        </button>
+        <button type="button" className="btn btn-outline-secondary">
+          Secondary
+        </button>
+        <button type="button" className="btn btn-outline-success">
+          Success
+        </button>
+        <button type="button" className="btn btn-outline-danger">
+          Danger
+        </button>
+        <button type="button" className="btn btn-outline-warning">
+          Warning
+        </button>
+        <button type="button" className="btn btn-outline-info">
+          Info
+        </button>
+        <button type="button" className="btn btn-outline-light">
+          Light
+        </button>
+        <button type="button" className="btn btn-outline-dark">
+          Dark
+        </button>
+      </div>
+      <p className={styles.copy__code__des}>
+        To achieve the above buttons, you need to follow the following code:
+      </p>
+      <CodeShare
+        highlight="1,3,5,7,9,11,13,15,17,19"
+        code={`
+                < -- For Primary Outline Button --> 
+        <button className="btn btn-outline-primary">Primary</button>
+                < -- For Secondary Outline Button -->
+        <button className="btn btn-outline-secondary">Secondary</button>
+                < -- For Success Outline Button -->
+        <button className="btn btn-outline-success">Success</button>
+                < -- For Warning Outline Button --> 
+        <button className="btn btn-outline-danger">Warning</button>
+                < -- For Danger Outline Button -->
+        <button className="btn btn-outline-warning">Danger</button>
+                < -- For Info Outline Button -->      
+        <button className="btn btn-outline-info">Info</button>
+                < -- For Light Outline Button -->
+        <button className="btn btn-outline-light">Light</button>
+                < -- For Dark Outline Button -->
+        <button className="btn btn-outline-dark">Dark</button>
+        `}
+      />
+      <h1>Button with link</h1>
+      <p className={styles.about__title}>
+        Button with link is used to link from one page to another. here href is
+        used to link to another page.
+      </p>
+      <div className={styles.button__showCase__container}>
+        <a href="/docs/button" type="button" className="btn btn-primary">
+          Primary
+        </a>
+        <a href="/docs/button" type="button" className="btn btn-success">
+          Success
+        </a>
+        <a href="/docs/button" type="button" className="btn btn-warning">
+          Warning
+        </a>
+        <a href="/docs/button" type="button" className="btn btn-dark">
+          Dark
+        </a>
+      </div>
+      <p className={styles.copy__code__des}>
+        To achieve the above buttons, you need to follow the following code:
+      </p>
+      <CodeShare
+        highlight="1,3,5,7,9,11,13,15,17,19"
+        code={`
+                < -- For Link Primary Button --> 
+        <a href="/docs/button" type="button" className="btn btn-primary">Primary</a>
+                < -- For Link Success Button -->
+        <a href="/docs/button" type="button" className="btn btn-success">Success</a>
+                < -- For Link Warning Button --> 
+        <a href="/docs/button" type="button" className="btn btn-warning">Warning</a>
+                < -- For Link Dark Button -->
+        <a href="/docs/button" type="button" className="btn btn-dark">Dark</a>
+        `}
+      />
+      <h1>Button with icon</h1>
+      <p className={styles.about__title}>
+        You can use Icons in your Buttons respectively.
+      </p>
+      <div className={styles.button__showCase__container}>
+        <button href="/docs/button" type="button" className="btn btn-primary">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-airplane-engines-fill"
+            viewBox="0 0 16 16"
+          >
+            <path d="M8 0c-.787 0-1.292.592-1.572 1.151A4.347 4.347 0 0 0 6 3v3.691l-2 1V7.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.191l-1.17.585A1.5 1.5 0 0 0 0 10.618V12a.5.5 0 0 0 .582.493l1.631-.272.313.937a.5.5 0 0 0 .948 0l.405-1.214 2.21-.369.375 2.253-1.318 1.318A.5.5 0 0 0 5.5 16h5a.5.5 0 0 0 .354-.854l-1.318-1.318.375-2.253 2.21.369.405 1.214a.5.5 0 0 0 .948 0l.313-.937 1.63.272A.5.5 0 0 0 16 12v-1.382a1.5 1.5 0 0 0-.83-1.342L14 8.691V7.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v.191l-2-1V3c0-.568-.14-1.271-.428-1.849C9.292.591 8.787 0 8 0Z" />
+          </svg>
+          Primary
+        </button>
+        <button href="/docs/button" type="button" className="btn btn-success">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-brightness-alt-high-fill"
+            viewBox="0 0 16 16"
+          >
+            <path d="M8 3a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 3zm8 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zm-13.5.5a.5.5 0 0 0 0-1h-2a.5.5 0 0 0 0 1h2zm11.157-6.157a.5.5 0 0 1 0 .707l-1.414 1.414a.5.5 0 1 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm-9.9 2.121a.5.5 0 0 0 .707-.707L3.05 5.343a.5.5 0 1 0-.707.707l1.414 1.414zM8 7a4 4 0 0 0-4 4 .5.5 0 0 0 .5.5h7a.5.5 0 0 0 .5-.5 4 4 0 0 0-4-4z" />
+          </svg>
+          Success
+        </button>
+        <button href="/docs/button" type="button" className="btn btn-warning">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-exclamation-octagon"
+            viewBox="0 0 16 16"
+          >
+            <path d="M4.54.146A.5.5 0 0 1 4.893 0h6.214a.5.5 0 0 1 .353.146l4.394 4.394a.5.5 0 0 1 .146.353v6.214a.5.5 0 0 1-.146.353l-4.394 4.394a.5.5 0 0 1-.353.146H4.893a.5.5 0 0 1-.353-.146L.146 11.46A.5.5 0 0 1 0 11.107V4.893a.5.5 0 0 1 .146-.353L4.54.146zM5.1 1 1 5.1v5.8L5.1 15h5.8l4.1-4.1V5.1L10.9 1H5.1z" />
+            <path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995z" />
+          </svg>
+          Warning
+        </button>
+        <button href="/docs/button" type="button" className="btn btn-dark">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-info-lg"
+            viewBox="0 0 16 16"
+          >
+            <path d="m9.708 6.075-3.024.379-.108.502.595.108c.387.093.464.232.38.619l-.975 4.577c-.255 1.183.14 1.74 1.067 1.74.72 0 1.554-.332 1.933-.789l.116-.549c-.263.232-.65.325-.905.325-.363 0-.494-.255-.402-.704l1.323-6.208Zm.091-2.755a1.32 1.32 0 1 1-2.64 0 1.32 1.32 0 0 1 2.64 0Z" />
+          </svg>
+          Dark
+        </button>
+      </div>
+      <p className={styles.copy__code__des}>
+        To achieve the above buttons, you need to follow the following code dont
+        forget to replace svg tag in button tag with your own svg tag. you can
+        use any svg,icons,img in button tag:
+      </p>
+      <CodeShare
+        highlight="1,3,5,7,9,11,13,15,17,19"
+        code={`
+                < -- For icon Primary Button --> 
+        <a href="/docs/button" type="button" className="btn btn-primary"> <svg/> Primary</a>
+                < -- For icon Success Button -->
+        <a href="/docs/button" type="button" className="btn btn-success"> <svg/> Success</a>
+                < -- For icon Warning Button --> 
+        <a href="/docs/button" type="button" className="btn btn-warning"> <svg/> Warning</a>
+                < -- For icon Dark Button -->
+        <a href="/docs/button" type="button" className="btn btn-dark"> <svg/> Dark</a>
+        `}
+      />
+      <h1>Disabled Button</h1>
+      <p className={styles.about__title}>
+        Make buttons look inactive by adding the .disabled class to any `button`
+        element. Disabled buttons have pointer-events: none applied to,
+        preventing hover and active states from triggering.
+      </p>
+      <div className={styles.button__showCase__container}>
+        <button type="button" className="btn btn-primary disabled">
+          Primary
+        </button>
+        <button type="button" className="btn btn-success disabled">
+          Success
+        </button>
+        <button type="button" className="btn btn-warning disabled">
+          Warning
+        </button>
+        <button type="button" className="btn btn-dark disabled">
+          Dark
+        </button>
+      </div>
+      <p className={styles.copy__code__des}>
+        To achieve the above buttons, you need to follow the following code:
+      </p>
+      <CodeShare
+        highlight="1,3,5,7,9,11,13,15,17,19"
+        code={`
+                < -- For Disabled Primary Button --> 
+        <button className="btn btn-primary disabled">Primary</button>
+                < -- For Disabled Success Button -->
+        <button className="btn btn-success disabled">Success</button>
+                < -- For Disabled Warning Button --> 
+        <button className="btn btn-warning disabled">Danger</button>
+                < -- For Disabled Dark Button -->
+        <button className="btn btn-dark disabled">Dark</button>
+        `}
+      />
+      <h1>Sizes</h1>
+      <p className={styles.about__title}>
+        Fancy larger or smaller buttons? Add .btn-lg or .btn-sm for additional
+        sizes.
+      </p>
+      <div className={styles.button__showCase__container}>
+        <button type="button" className="btn btn-primary btn-lg">
+          Primary
+        </button>
+        <button type="button" className="btn btn-success btn-lg">
+          Success
+        </button>
+        <button type="button" className="btn btn-warning btn-lg">
+          Warning
+        </button>
+        <button type="button" className="btn btn-dark btn-lg">
+          Dark
+        </button>
+      </div>
+      <p className={styles.copy__code__des}>
+        To achieve the above buttons, you need to follow the following code:
+      </p>
+      <CodeShare
+        highlight="1,3,5,7,9,11,13,15,17,19"
+        code={`
+                < -- For Larger Size Primary Button --> 
+        <button className="btn btn-primary btn-lg">Primary</button>
+                < -- For Larger Size Success Button -->
+        <button className="btn btn-success btn-lg">Success</button>
+                < -- For Larger Size Warning Button --> 
+        <button className="btn btn-warning btn-lg">Danger</button>
+                < -- For Larger Size Dark Button -->
+        <button className="btn btn-dark btn-lg">Dark</button>
+        `}
+      />
+      <div className={styles.button__showCase__container}>
+        <button type="button" className="btn btn-primary btn-sm">
+          Primary
+        </button>
+        <button type="button" className="btn btn-success btn-sm">
+          Success
+        </button>
+        <button type="button" className="btn btn-warning btn-sm">
+          Warning
+        </button>
+        <button type="button" className="btn btn-dark btn-sm">
+          Dark
+        </button>
+      </div>
+      <p className={styles.copy__code__des}>
+        To achieve the above buttons, you need to follow the following code:
+      </p>
+      <CodeShare
+        highlight="1,3,5,7,9,11,13,15,17,19"
+        code={`
+                < -- For Small Size Primary Button --> 
+        <button className="btn btn-primary btn-sm">Primary</button>
+                < -- For Small Size Success Button -->
+        <button className="btn btn-success btn-sm">Success</button>
+                < -- For Small Size Warning Button --> 
+        <button className="btn btn-warning btn-sm">Danger</button>
+                < -- For Small Size Dark Button -->
+        <button className="btn btn-dark btn-sm">Dark</button>
         `}
       />
     </div>
